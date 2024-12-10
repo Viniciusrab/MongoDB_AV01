a3ba4f081519bb5b1c7eb4be9ce44f6a2774a5ce
Viniciusrab <vinicius.raphael@estudante.iftm.edu.br>
2024-12-09 22:44:38 -0300

    Ajustes diversos

--- src/main/java/com/iftm/edu/br/product_api/controller/ProductController.java
+++ src/main/java/com/iftm/edu/br/product_api/controller/ProductController.java
@@ -1,5 +1,52 @@
 package com.iftm.edu.br.product_api.controller;
 
+import com.iftm.edu.br.product_api.models.Product;
+import com.iftm.edu.br.product_api.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
-    
+
+    @Autowired
+    private ProductService productService;
+
+    // Endpoint para salvar um produto
+    @PostMapping
+    public ResponseEntity<Product> saveProduct(@RequestBody Product product) {
+        Product savedProduct = productService.save(product);
+        return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+    }
+
+    // Endpoint para buscar todos os produtos
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.findAll();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    // Endpoint para buscar um produto por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable String id) {
+        Optional<Product> product = productService.findById(id);
+        if (product.isPresent()) {
+            return new ResponseEntity<>(product.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Endpoint para buscar produtos por categoria
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<Product>> getProductsByCategory(@PathVariable String category) {
+        List<Product> products = productService.findByCategory(category);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
 }
+
